3ebb6263c33cd9231891fcf70f3c42f87f54d526
Mel Sawyer <melaniensawyer@gmail.com>
2022-09-25 19:26:37 -0700

    Updates

--- src/components/Origami.jsx
+++ src/components/Origami.jsx
@@ -14,6 +14,19 @@ import ninth from '../img/9.png';
 import tenth from '../img/10.png';
 import eleventh from '../img/11.png';
 import twelfth from '../img/12.png';
+import thirteenth from '../img/13.png';
+import fourteenth from '../img/14.png';
+import fifteenth from '../img/15.png';
+import sixteenth from '../img/16.png';
+import seventeenth from '../img/17.png';
+import eighteenth from '../img/18.png';
+import nineteenth from '../img/19.png';
+import twenty from '../img/20.png';
+import twentyOne from '../img/21.png';
+import twentyTwo from '../img/22.png';
+import twentyThree from '../img/23.png';
+import twentyFour from '../img/24.png';
+
 export default function Origami() {
 
     return <div> 
@@ -22,24 +35,44 @@ export default function Origami() {
         <div className="centered mt-1 mb-3">Here's a collection of models I've made over the years. Click on each image for information and the geometry of each model. For more, <a href="https://www.instagram.com/msorigami" className="unlink highlight-d-prp" target="_blank">check out my Instagram!</a></div>
         <Container>
         <Row>
-            <OrigamiImage src={first} title="Bascetta Star #1" id={1}/>
-            <OrigamiImage src={second} title="Bascetta Star #1" id={2}/>
-            <OrigamiImage src={third} title="Bascetta Star #1" id={3}/>
+            <OrigamiImage src={first} id={1}/>
+            <OrigamiImage src={second} id={2}/>
+            <OrigamiImage src={third} id={3}/>
         </Row>
         <Row>
-            <OrigamiImage src={fourth} title="Bascetta Star #1" id={4} />
-            <OrigamiImage src={fifth} title="Bascetta Star #1" id={5}/>
-            <OrigamiImage src={sixth} title="Bascetta Star #1" id={6}/>
+            <OrigamiImage src={fourth} id={4} />
+            <OrigamiImage src={fifth} id={5}/>
+            <OrigamiImage src={sixth} id={6}/>
         </Row>
         <Row>
-            <OrigamiImage src={seventh} title="Bascetta Star #1" id={7}/>
-            <OrigamiImage src={eighth} title="Bascetta Star #1" id={8}/>
-            <OrigamiImage src={ninth} title="Bascetta Star #1" id={9}/>
+            <OrigamiImage src={seventh} id={7}/>
+            <OrigamiImage src={eighth} id={8}/>
+            <OrigamiImage src={ninth} id={9}/>
         </Row>
         <Row>
-            <OrigamiImage src={tenth} title="Bascetta Star #1" id={10}/>
-            <OrigamiImage src={eleventh} title="Bascetta Star #1" id={11}/>
-            <OrigamiImage src={twelfth} title="Bascetta Star #1" id={12}/>
+            <OrigamiImage src={tenth} id={10}/>
+            <OrigamiImage src={eleventh} id={11}/>
+            <OrigamiImage src={twelfth} id={12}/>
+        </Row>
+        <Row>
+            <OrigamiImage src={thirteenth} id={13}/>
+            <OrigamiImage src={fourteenth} id={14}/>
+            <OrigamiImage src={fifteenth} id={15}/>
+        </Row>
+        <Row>
+            <OrigamiImage src={sixteenth} id={16}/>
+            <OrigamiImage src={seventeenth} id={17}/>
+            <OrigamiImage src={eighteenth} id={18}/>
+        </Row>
+        <Row>
+            <OrigamiImage src={nineteenth} id={19}/>
+            <OrigamiImage src={twenty} id={20}/>
+            <OrigamiImage src={twentyOne} id={21}/>
+        </Row>
+        <Row>
+            <OrigamiImage src={twentyTwo} id={22}/>
+            <OrigamiImage src={twentyThree} id={23}/>
+            <OrigamiImage src={twentyFour} id={24}/>
         </Row>
 
     </Container>
--- src/components/Subcomponents/OrigamiImages.jsx
+++ src/components/Subcomponents/OrigamiImages.jsx
@@ -5,6 +5,25 @@ import img_4 from '../../img/4.png';
 import img_5 from '../../img/5.png';
 import img_6 from '../../img/6.png';
 import img_7 from '../../img/7.png';
+import img_8 from '../../img/8.png';
+import img_9 from '../../img/9.png';
+import img_10 from '../../img/10.png';
+import img_11 from '../../img/11.png';
+import img_12 from '../../img/12.png';
+import img_13 from '../../img/13.png';
+import img_14 from '../../img/14.png';
+import img_15 from '../../img/15.png';
+import img_16 from '../../img/16.png';
+import img_17 from '../../img/17.png';
+import img_18 from '../../img/18.png';
+import img_19 from '../../img/19.png';
+import img_20 from '../../img/20.png';
+import img_21 from '../../img/21.png';
+import img_22 from '../../img/22.png';
+import img_23 from '../../img/23.png';
+import img_24 from '../../img/24.png';
+
+
 
 export class OrigamiImages{
 
@@ -21,6 +40,41 @@ export class OrigamiImages{
     6 = img_6
 
     7 = img_7
+
+    8 = img_8
+
+    9 = img_9
+
+    10 = img_10
+
+    11 = img_11
+
+    12 = img_12
+
+    13 = img_13
+
+    14 = img_14
+
+    15 = img_15
+
+    16 = img_16
+
+    17 = img_17
+
+    18 = img_18
+
+    19 = img_19
+
+    20 = img_20
+
+    21 = img_21
+
+    22 = img_22
+
+    23 = img_23
+
+    24 = img_24
+
 
   }
   export default OrigamiImages;
